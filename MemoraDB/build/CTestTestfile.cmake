5b2c2f899073642b47058b32237fa8ba3706862d
CircumFlexing Coon <47142967+Baum97@users.noreply.github.com>
2023-04-30 20:49:46 +0200

    initial upload

---
+++ MemoraDB/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /workspaces/MemoraDB
+# Build directory: /workspaces/MemoraDB/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("server")
+subdirs("lib")
+subdirs("client")
+subdirs("tests")
